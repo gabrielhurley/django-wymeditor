bd4841c9fac3e776cfafefcc05fdc87a1d31c158
Gabriel Hurley <gabriel@strikeawe.com>
2011-04-15 18:56:27 -0700

    Fixed a missing colon and bad indent.

--- wymeditor/models.py
+++ wymeditor/models.py
@@ -9,6 +9,6 @@ class WYMEditorField(models.TextField):
         defaults.update(kwargs)
         return super(WYMEditorField, self).formfield(**defaults)
 
-if 'south' in settings.INSTALLED_APPS
+if 'south' in settings.INSTALLED_APPS:
     from south.modelsinspector import add_introspection_rules
-        add_introspection_rules([], ["^wymeditor\.models\.WYMEditorField"])
+    add_introspection_rules([], ["^wymeditor\.models\.WYMEditorField"])
