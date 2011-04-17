0969f2bf484ba12525f45585300dc88a2ba228eb
Gabriel Hurley <gabriel@strikeawe.com>
2011-04-17 16:04:22 -0700

    Using setuptools instead of distutils.

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='django-wymeditor',
     version='1.0',
     description='A Django application that contains a widget to render a form field with a WYMEditor interface.',
+    long_description=open('README.rst').read(),
     author='Gabriel Hurley',
     author_email='gabriel@strikeawe.com',
+    license='BSD',
     url='https://github.com/gabrielhurley/django-wymeditor',
     download_url='git://github.com/gabrielhurley/django-wymeditor.git',
-    packages=['wymeditor',],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
